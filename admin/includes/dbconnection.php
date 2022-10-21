ad2c19cfe3f2d7c5a5e6706502115dc97d8b67b5
chobela <chobelak@gmail.com>
2022-10-22 00:06:50 +0200

    first commit

---
+++ admin/includes/dbconnection.php
@@ -0,0 +1,7 @@
+<?php
+$con=mysqli_connect("localhost", "root", "Theres@1#", "bpmsdb");
+if(mysqli_connect_errno()){
+echo "Connection Fail".mysqli_connect_error();
+}
+
+  ?>
---
+++ header.php
@@ -0,0 +1,10 @@
+<nav class="main_nav">
+				<ul class="d-flex flex-row align-items-start justify-content-start">
+					<li><a href="index.php">Home<div><div></div><div></div><div></div></div></a></li>
+					<li><a href="about.php">About<div><div></div><div></div><div></div></div></a></liclass=>
+					<li><a href="services.php">Services<div><div></div><div></div><div></div></div></a></li>
+					
+					<li><a href="contact.php">Contact<div><div></div><div></div><div></div></div></a></li>
+					<li><a href="admin/index.php">Admin<div><div></div><div></div><div></div></div></a></li>
+				</ul>
+			</nav>
