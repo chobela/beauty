22d4372a7eb1b8806ed931b423c647f206da43c0
unknown <josephphiri909@gmail.com>
2022-10-24 15:09:34 +0200

    Edit content

--- signup.php
+++ signup.php
@@ -22,8 +22,8 @@
 
 			<!-- Logo -->
 			<div class="logo">
-				<a href="#">
-					<div>V<span>o</span>gue</div>
+			<a href="#">
+					<div>B<span>ea</span>uty</div>
 					<div>hair</div>
 				</a>
 				<div class="hamburger"><div></div><div></div><div></div></div>
@@ -147,7 +147,7 @@
 		</div>
 		<div class="footer_bar d-flex flex-row align-items-center justify-content-start">
 			<div class="copyright"><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="fa fa-heart-o" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
+Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved 
 <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></div>
 		</div>
 	</footer>
